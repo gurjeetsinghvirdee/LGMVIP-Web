1ffc48fca042725320f9ee97c2eed9e2d0291da8
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-02-02 00:27:47 +0530

    button fixed & color chnged

--- ToDo App/main.js
+++ ToDo App/main.js
@@ -5,17 +5,17 @@ const todoList = document.getElementById("todoList");
 addTodo.addEventListener("click", function() {
     if (todoInput.value === "") return;
 
-    const todoItem = document.createElement("li");
+    const todoItem = document.createElement("span");
     todoItem.classList.add("todo-item");
     todoItem.innerHTML = `
         ${todoInput.value}
-        <button class="delete">Delete</button>
+        <button class="done">Done</button>
     `;
     todoList.appendChild(todoItem);
     todoInput.value = "";
 
-    const deleteButton = todoItem.querySelector(".delete");
-    deleteButton.addEventListener("click", function() {
+    const doneButton = todoItem.querySelector(".done");
+    doneButton.addEventListener("click", function() {
         todoItem.remove();
     });
 
